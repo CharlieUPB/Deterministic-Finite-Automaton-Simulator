df252ef3cacf112eee659a7d02083297629c86eb
Carlos Gamboa <carlos.gamboa@fundacion-jala.org>
2019-06-22 20:46:15 -0400

    Fix arrow drawing.

--- src/application/sceneAutomataController.java
+++ src/application/sceneAutomataController.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Optional;
 import java.util.Stack;
+import java.util.function.ObjDoubleConsumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.animation.FadeTransition;
@@ -68,7 +69,7 @@ public class sceneAutomataController {
 
 	@FXML 
 	private Tab inicioTab;
-	
+
 	@FXML
 	private Button createEditButton;
 
@@ -92,61 +93,68 @@ public class sceneAutomataController {
 
 	@FXML 
 	private Tab automataTab;
-	
+
 	@FXML
 	private AnchorPane drawAreaAnchorPane;
-	
+
 	@FXML
 	private Button newStateButton;
-	
+
 	@FXML
 	private Button newTransitionButton;
-	
+
 	@FXML
 	private Button deleteButton;
-	
-	
+
+
 	@FXML
 	private TextField inputString;
-        
-    @FXML
-    private Label labelCurrentInput;
 
-    @FXML
+	@FXML
+	private Label labelCurrentInput;
+
+	@FXML
 	private Button btnStart;
 
 	@FXML
 	private AnchorPane inputCadenaEntrada;
-	
+
 	@FXML
 	private AnchorPane pane;
-	
-	
+
+
 	ArrayList<State> stateArray = new ArrayList<State>();
 	ArrayList<Transition> transitionArray = new ArrayList<Transition>();
 	ArrayList<State> acceptanceStatesArray = new ArrayList<State>();
 	private Automata automata = new Automata(stateArray, null , acceptanceStatesArray, transitionArray);
-	
+
 	ToggleGroup toggleGroup = new ToggleGroup();
 
 	FileManager fManager = new FileManager();
-	
+
+	State initialState;
+	State nextState;
+
+	double firstClickedX = 0;
+	double firstClickedY = 0;
+	double secondClickedX = 0;
+	double secondClickedY = 0;
+
+	char transitionSymbol;
+	String stateName;
+
 	private enum editionType
 	{
 		NEWSTATE, 
 		NEWTRANSITION,
 		DELETE;
 	}
-	
+
 	editionType currentEditionType;
-	
-	int numClicksTransition = 0;
 
-	public sceneAutomataController()
-	{
+	int numberOfTransitionClicks = 0;
 
-	}
-
+	public sceneAutomataController() {}
 
 	@FXML
 	private void initialize()
@@ -155,6 +163,129 @@ public class sceneAutomataController {
 		//this.labelCurrentInput.setTooltip(new Tooltip("Entrada"));
 	}
 
+	@FXML
+	public void createAutomatonOnMouseClicked(MouseEvent event)
+	{
+		switch (this.currentEditionType) 
+		{
+		case NEWSTATE:
+			this.createNewState(event);
+			break;
+
+		case NEWTRANSITION:
+			this.createNewTransition(event);
+			break;
+
+		case DELETE:
+
+			break;
+
+		default:
+			break;
+		}
+
+	}
+
+	public void createNewState(MouseEvent event)
+	{
+		double coordX = event.getX();
+		double coordY = event.getY();
+		if (this.isStateInsidePane(coordX, coordY))
+		{
+			if (!this.automata.DoesStateCollision(coordX, coordY))
+			{
+				Circle circle = new Circle(coordX,coordY, State.RADIUS, javafx.scene.paint.Color.BLUE);
+				this.drawAreaAnchorPane.getChildren().add(circle);
+
+				State state = new State(this.stateName, coordX, coordY);
+				this.stateArray.add(state);
+			}
+		}
+	}
+
+	public void createNewTransition(MouseEvent event) 
+	{ 
+		double coordX = event.getX();
+		double coordY = event.getY();
+
+		State clickedState = this.getStateByCoords(coordX, coordY);
+
+		if (clickedState != null)
+		{
+			this.numberOfTransitionClicks += 1;
+
+			if (this.numberOfTransitionClicks == 1)
+			{
+				this.initialState = clickedState;
+				this.firstClickedX = coordX;
+				this.firstClickedY = coordY;
+			}
+			else if (this.numberOfTransitionClicks == 2)
+			{
+				this.nextState = clickedState;
+				this.secondClickedX = coordX;
+				this.secondClickedY = coordY;
+
+				this.createTransitionArrow(this.firstClickedX,this.firstClickedY, this.secondClickedX, this.secondClickedY);
+
+				Transition transition = new Transition(this.transitionSymbol, this.initialState, this.nextState);
+				this.transitionArray.add(transition);
+
+				this.numberOfTransitionClicks = 0;
+			}
+		}
+		else 
+		{
+			this.numberOfTransitionClicks = 0;
+		}
+
+	}
+
+	public void createTransitionArrow(double x0, double y0, double x1, double y1)
+	{
+		Arrow arrow = new Arrow();
+		arrow.setStartX(x0);
+		arrow.setStartY(y0);
+		arrow.setEndX(x1);
+		arrow.setEndY(y1);
+
+		this.drawAreaAnchorPane.getChildren().add(arrow);
+	}
+
+	private boolean isStateInsidePane(double coordX, double coordY) 
+	{
+		if (this.drawAreaAnchorPane.getWidth() > (coordX + State.RADIUS) && 
+				this.drawAreaAnchorPane.getHeight() > (coordY + State.RADIUS) &&
+				(coordX - State.RADIUS >= 0) && (coordY - State.RADIUS >= 0))
+		{
+			return true;
+		} 
+		else 
+		{
+			return false;
+		}
+	}
+
+	public void setEditionVariableState()
+	{
+		this.currentEditionType = editionType.NEWSTATE;
+	}
+
+	public void setEditionVariableTransition()
+	{
+		this.currentEditionType = editionType.NEWTRANSITION;
+		this.numberOfTransitionClicks = 0;
+	}
+
+	public void setEditionVariableDelete()
+	{
+		this.currentEditionType = editionType.DELETE;
+	}
+
+	public State getStateByCoords(double coordX, double coordY)
+	{
+		return this.automata.getStateByCoords(coordX, coordY);
+	}
 
 	//Called when a file is double clicked from the main menu
 
@@ -170,7 +301,7 @@ public class sceneAutomataController {
 			this.mainTab.getSelectionModel().selectNext();
 			try {
 				//this.automata = this.fm.getAutomata(fileSelected);
-                //this.loadResources(this.automata);
+				//this.loadResources(this.automata);
 			} catch (Exception ex) {
 				Alert errorAlert = new Alert(AlertType.ERROR);
 				errorAlert.setHeaderText("Oops! There was a problem");
@@ -179,195 +310,6 @@ public class sceneAutomataController {
 			}
 		}
 	}
-
-	
-	
-	
-	
-	
-	
-	@FXML
-	public void createAutomatonOnMouseClicked(MouseEvent event)
-	{
-		switch (this.currentEditionType) 
-		{
-			case NEWSTATE:
-				this.createNewState(event);
-			break;
-			
-			case NEWTRANSITION:
-				double coordX = event.getX();
-				double coordY = event.getY();
-				if(this.getStateByCoords(coordX, coordY) != null)
-				{
-					System.out.println("Clickeaste un state");
-				}
-				else 
-				{
-					System.out.println("NO ES UN STATE.");
-        }
-        
-				if(event.getClickCount() == 2)
-				{
-					createNewTransition(event);
-				}
-			break;
-				
-			case DELETE:
-				
-			break;
-
-			default:
-			break;
-		}
-		
-	}
-	
-	public State getStateByCoords(double coordX, double coordY)
-	{
-		return this.automata.getStateByCoords(coordX, coordY);
-	}
-	
-	public void createNewState(MouseEvent event)
-	{
-		double coordX = event.getX();
-		double coordY = event.getY();
-		if (this.isStateInsidePane(coordX, coordY))
-		{
-			if (!this.automata.DoesStateCollision(coordX, coordY))
-			{
-				State state = new State("testy", coordX, coordY);
-				this.stateArray.add(state);
-				Circle circle = new Circle(coordX,coordY, State.RADIUS, javafx.scene.paint.Color.BLUE);
-				this.drawAreaAnchorPane.getChildren().add(circle);
-			}
-		}
-	}
-	
-	private boolean isStateInsidePane(double coordX, double coordY) 
-	{
-		if (this.drawAreaAnchorPane.getWidth() > (coordX + State.RADIUS) && 
-			this.drawAreaAnchorPane.getHeight() > (coordY + State.RADIUS) &&
-		    (coordX - State.RADIUS >= 0) && (coordY - State.RADIUS >= 0))
-		{
-			return true;
-		} 
-		else 
-		{
-			return false;
-		}
-	}
-	
-	public void setEditionVariableState()
-	{
-		this.currentEditionType = editionType.NEWSTATE;
-	}
-	
-	public void setEditionVariableTransition()
-	{
-		this.currentEditionType = editionType.NEWTRANSITION;
-	}
-	
-	public void setEditionVariableDelete()
-	{
-		this.currentEditionType = editionType.DELETE;
-	}
-	
-	
-
-	public void createNewTransition(MouseEvent event) 
-	{
-		double xIni = 0;
-		double yIni = 0;
-		double xFinal = 0;
-		double yFinal = 0;
-		
-		if(event.getClickCount() == 1)
-		{
-			xIni = event.getX();
-			yIni = event.getY();
-		}
-		else if (event.getClickCount() == 2) 
-		{
-			xFinal = event.getX();
-			yFinal = event.getY();
-		}
-
-
-	    CubicCurve curve1 = new CubicCurve(xIni, yIni, 125, 225, 325, 225, xFinal, yFinal);
-	    curve1.setStroke(javafx.scene.paint.Color.BLACK);
-	    curve1.setStrokeWidth(1);
-	    curve1.setFill( null);
-
-	    double size=Math.max(curve1.getBoundsInLocal().getWidth(),
-	                         curve1.getBoundsInLocal().getHeight());
-	    double scale=size/4d;
-
-	    Point2D ori=eval(curve1,0);
-	    Point2D tan=evalDt(curve1,0).normalize().multiply(scale);
-	    Path arrowIni=new Path();
-	    arrowIni.getElements().add(new MoveTo(ori.getX()+0.2*tan.getX()-0.2*tan.getY(),
-	                                        ori.getY()+0.2*tan.getY()+0.2*tan.getX()));
-	    arrowIni.getElements().add(new LineTo(ori.getX(), ori.getY()));
-	    arrowIni.getElements().add(new LineTo(ori.getX()+0.2*tan.getX()+0.2*tan.getY(),
-	                                        ori.getY()+0.2*tan.getY()-0.2*tan.getX()));
-
-	    ori=eval(curve1,1);
-	    tan=evalDt(curve1,1).normalize().multiply(scale);
-	    Path arrowEnd=new Path();
-	    arrowEnd.getElements().add(new MoveTo(ori.getX()-0.2*tan.getX()-0.2*tan.getY(),
-	                                        ori.getY()-0.2*tan.getY()+0.2*tan.getX()));
-	    arrowEnd.getElements().add(new LineTo(ori.getX(), ori.getY()));
-	    arrowEnd.getElements().add(new LineTo(ori.getX()-0.2*tan.getX()+0.2*tan.getY(),
-	                                        ori.getY()-0.2*tan.getY()-0.2*tan.getX()));
-	    
-	    this.drawAreaAnchorPane.getChildren().addAll(curve1, arrowIni, arrowEnd);
-	    
-	}
-
-	/**
-	 * Evaluate the cubic curve at a parameter 0<=t<=1, returns a Point2D
-	 * @param c the CubicCurve 
-	 * @param t param between 0 and 1
-	 * @return a Point2D 
-	 */
-	private Point2D eval(CubicCurve c, float t){
-	    Point2D p=new Point2D(Math.pow(1-t,3)*c.getStartX()+
-	            3*t*Math.pow(1-t,2)*c.getControlX1()+
-	            3*(1-t)*t*t*c.getControlX2()+
-	            Math.pow(t, 3)*c.getEndX(),
-	            Math.pow(1-t,3)*c.getStartY()+
-	            3*t*Math.pow(1-t, 2)*c.getControlY1()+
-	            3*(1-t)*t*t*c.getControlY2()+
-	            Math.pow(t, 3)*c.getEndY());
-	    return p;
-	}
-
-	/**
-	 * Evaluate the tangent of the cubic curve at a parameter 0<=t<=1, returns a Point2D
-	 * @param c the CubicCurve 
-	 * @param t param between 0 and 1
-	 * @return a Point2D 
-	 */
-	private Point2D evalDt(CubicCurve c, float t){
-	    Point2D p=new Point2D(-3*Math.pow(1-t,2)*c.getStartX()+
-	            3*(Math.pow(1-t, 2)-2*t*(1-t))*c.getControlX1()+
-	            3*((1-t)*2*t-t*t)*c.getControlX2()+
-	            3*Math.pow(t, 2)*c.getEndX(),
-	            -3*Math.pow(1-t,2)*c.getStartY()+
-	            3*(Math.pow(1-t, 2)-2*t*(1-t))*c.getControlY1()+
-	            3*((1-t)*2*t-t*t)*c.getControlY2()+
-	            3*Math.pow(t, 2)*c.getEndY());
-	    return p;
-	}
-	
-	
-	
-
-	
-	
-	
-	
 
 	//Called when openFile from menu is clicked
 
@@ -390,13 +332,13 @@ public class sceneAutomataController {
 		}
 
 	}
-        
-        @FXML 
-        public void createNewFile() {
-        	//this.automata = new Automata();
-        	this.inputString.setText("");
-        	this.mainTab.getSelectionModel().selectFirst();
-        }
+
+	@FXML 
+	public void createNewFile() {
+		//this.automata = new Automata();
+		this.inputString.setText("");
+		this.mainTab.getSelectionModel().selectFirst();
+	}
 
 	@FXML
 	public void saveFile() {
@@ -425,23 +367,23 @@ public class sceneAutomataController {
 	public void reRender() {
 		this.loadRecentFiles();
 	}
-        
-        private void loadRecentFiles() {
-        	this.recentFilesList.getItems().clear();
-        	this.menuOpenFile.getItems().clear();
-        	try {
-        		Files.newDirectoryStream(Paths.get("./savedData"),path -> path.toString().endsWith(".json"))
-        		.forEach(filePath -> { 
-        			RadioMenuItem r1 = new RadioMenuItem(filePath.getFileName().toString());
-        			r1.setToggleGroup(this.toggleGroup);
-        			this.recentFilesList.getItems().add(filePath.getFileName());
-        			this.menuOpenFile.getItems().add(r1);
-        		});
-        	} catch (IOException ex) {
-        		Alert errorAlert = new Alert(AlertType.ERROR);
-        		errorAlert.setHeaderText("No files found");
-        		errorAlert.setContentText(ex.toString());
-        		errorAlert.showAndWait();
+
+	private void loadRecentFiles() {
+		this.recentFilesList.getItems().clear();
+		this.menuOpenFile.getItems().clear();
+		try {
+			Files.newDirectoryStream(Paths.get("./savedData"),path -> path.toString().endsWith(".json"))
+			.forEach(filePath -> { 
+				RadioMenuItem r1 = new RadioMenuItem(filePath.getFileName().toString());
+				r1.setToggleGroup(this.toggleGroup);
+				this.recentFilesList.getItems().add(filePath.getFileName());
+				this.menuOpenFile.getItems().add(r1);
+			});
+		} catch (IOException ex) {
+			Alert errorAlert = new Alert(AlertType.ERROR);
+			errorAlert.setHeaderText("No files found");
+			errorAlert.setContentText(ex.toString());
+			errorAlert.showAndWait();
 		}
 	}
 
@@ -455,23 +397,23 @@ public class sceneAutomataController {
 			this.inputString.setDisable(true);
 			this.btnStart.getStyleClass().set(1, "danger");
 			this.btnStart.setText("Stop");
-              
-                        // TEST WORD
-                        //System.out.println(possible);
-                        if(true) {
-                             this.animate();
-                        } else {
-                            Alert alert = new Alert(AlertType.ERROR);
-                            alert.setTitle("ERROR!");
-                            alert.setHeaderText("NO SE ENCONTRO SOLUCION");
-                            alert.setContentText("La cadena de entrada no pertenece al lenguaje");
-                            alert.showAndWait();
-                            this.topMenu.setDisable(false);
-                            this.inicioTab.setDisable(false);
-                            this.inputString.setDisable(false);
-                            this.btnStart.getStyleClass().set(1, "success");
-                            this.btnStart.setText("Start");
-                        }
+
+			// TEST WORD
+			//System.out.println(possible);
+			if(true) {
+				this.animate();
+			} else {
+				Alert alert = new Alert(AlertType.ERROR);
+				alert.setTitle("ERROR!");
+				alert.setHeaderText("NO SE ENCONTRO SOLUCION");
+				alert.setContentText("La cadena de entrada no pertenece al lenguaje");
+				alert.showAndWait();
+				this.topMenu.setDisable(false);
+				this.inicioTab.setDisable(false);
+				this.inputString.setDisable(false);
+				this.btnStart.getStyleClass().set(1, "success");
+				this.btnStart.setText("Start");
+			}
 		} else {
 			this.topMenu.setDisable(false);
 			this.inicioTab.setDisable(false);
@@ -479,28 +421,28 @@ public class sceneAutomataController {
 			this.btnStart.getStyleClass().set(1, "success");
 			this.btnStart.setText("Start");
 		}
-                
-               
+
+
 	}
 
-        
-        private void animate() {
-           
-        }
-        
-        @FXML
-    	public void showEditComponent() {
-    	}
-        
-        @FXML 
-        public void showAbout() {
-            Alert alert = new Alert(AlertType.INFORMATION);
-                            alert.setTitle("About US!");
-                            alert.setHeaderText("Look for helps in our repo");
-                            alert.setContentText("Git Repository: https://github.com/Eli-C/Pushdown-Automata-Simulator \n Feel Free to Open Issues, or give recommendatios \n The Team: Elionor Cordova \n - Eun Kyu Choi \n - Carlos Gamboa" );
-                            
-                            Platform.runLater(alert::showAndWait);
-        }
+
+	private void animate() {
+
+	}
+
+	@FXML
+	public void showEditComponent() {
+	}
+
+	@FXML 
+	public void showAbout() {
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("About US!");
+		alert.setHeaderText("Look for helps in our repo");
+		alert.setContentText("Git Repository: https://github.com/Eli-C/Pushdown-Automata-Simulator \n Feel Free to Open Issues, or give recommendatios \n The Team: Elionor Cordova \n - Eun Kyu Choi \n - Carlos Gamboa" );
+
+		Platform.runLater(alert::showAndWait);
+	}
 
 
 	public void ErrorAlertMessage()
